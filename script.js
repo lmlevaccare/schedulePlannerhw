187475daa23366bcc51832e1c3647a79009d9510
Lauren Levaccare <laurenlevaccare@icloud.com>
2020-11-04 17:37:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
+
 // header
 var m = moment()
 
 console.log(m.format("MMMM Do YYYY"));
-var m2 = (m.format("MMMM Do YYYY"));
+// var m2 = (m.format("MMMM Do YYYY"));
 
-console.log(m2)
+var cDate = document.getElementById("curDate");
+    
+cDate.append(m.format("MMMM Do YYYY"));
 
-var m2 = document.getElementById("#curDate");
+var curTim = document.getElementById("time");
 
-curDate.append(m.format("MMMM Do YYYY"));
-
-var timeBlock = (m.format(" h m A"));
-var timeBlock = document.getElementById("#time");
-
-time.append(m.format(" h:mm A")); 
-console.log(timeBlock)
-
-// body of calendar timeblock
-var colorTime=document.getElementById("#i")
-
-var presentT =moment().format('h:mm:ss a');
-console.log(presentT)
-
-var futureT=moment().endOf('day').fromNow();    
-console.log(futureT)
-
-var pastT = moment().subtract(1, 'hour').calendar(); 
-console.log(pastT)
-
-var yellow = $(".present");
-var red = $('.past');
-var green = $('.future');
+curTim.append(m.format(" h:mm A")); 
 
 
+// TIMEBLOCK COLOR CHANGE
+
+var timeBlck = document.getElementsByClassName("time-block");
+ 
+
+$.each(timeBlck, function () {
+    var blcknumb = parseInt($(this).attr("data-time"));
+    console.log(blcknumb);
+    console.log(moment().hours());
 
 
+    if (blcknumb < moment().hours()) {
+        $(this).addClass("past");
 
-   function mySave() {
+
+    } else if (blcknumb > moment().hours()) {
+        $(this).addClass("future");
+      
+    }
+
+    else if (blcknumb === moment().hours()) 
+        $(this).addClass("present");
+      
+    });
+
+    // LOCAL STORAGE EVENTS
+ 
+        function mySave() {
         var myContent = document.getElementById("myTextarea").value;
         localStorage.setItem("myContent", myContent);
       }
@@ -45,39 +50,4 @@ var green = $('.future');
         document.getElementById("myTextarea").value = myContent;
       }
 
-
-
-  
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if (key && value) {
-//     localStorage.setItem("text", "storage");
-//     location.reload();
-// }
-//     })
-
-// for(let i=0; i<localStorage.length; i++) {
-//     const key = localStorage.key(i);
-//     const value = localStorage.getItem(key);
-//     isOutput.innerHTML += '${key}: ${value}<br />';
-    
-
-// }
 
